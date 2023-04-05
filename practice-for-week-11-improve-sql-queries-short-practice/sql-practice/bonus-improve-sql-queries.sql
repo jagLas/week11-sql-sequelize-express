a6cfba946612e2660afd8ed7317ee44610c70bd1
jagLas <jglasher@gmail.com>
2023-04-05 12:25:52 -0400

    practice complete

--- practice-for-week-11-improve-sql-queries-short-practice/sql-practice/bonus-improve-sql-queries.sql
+++ practice-for-week-11-improve-sql-queries-short-practice/sql-practice/bonus-improve-sql-queries.sql
@@ -5,10 +5,33 @@
     -- 55 and belong to a cat that has the color "Olive".
 
     -- Your code here
+-- SELECT COUNT(*) FROM toys
+--     WHERE price > 55 AND id IN (
+--         SELECT toy_id FROM cat_toys JOIN cats ON cats.id = cat_id
+--             WHERE color = 'Olive'
+--     ); 
+
 
 -- Paste your results below (as a comment):
 
+-- 212
+-- readme indicates that it should be 215, but I would argue that is only true if you are
+-- selecting a count of all toys, not toys records
 
+-- The query they probably wanted was
+-- SELECT COUNT(*) FROM cat_toys JOIN toys ON toy_id = toys.id
+--     JOIN cats ON cats.id = cat_id
+--     WHERE price > 55 AND cats.color = 'Olive';
+
+-- which yields 215
+-- or instead using subqueries
+
+-- SELECT COUNT(*) FROM cat_toys 
+--     WHERE cat_id IN (
+--         SELECT id FROM cats WHERE color = 'Olive'
+--     ) AND toy_id IN (
+--         SELECT id FROM toys WHERE price > 55
+--     );
 
 
 ----------
@@ -17,16 +40,29 @@
 -- Query:
 
     -- Your code here
+-- EXPLAIN QUERY PLAN 
+-- SELECT COUNT(*) FROM cat_toys 
+--     WHERE cat_id IN (
+--         SELECT id FROM cats WHERE color = 'Olive'
+--     ) AND toy_id IN (
+--         SELECT id FROM toys WHERE price > 55
+--     );
 
 -- Paste your results below (as a comment):
-
+-- QUERY PLAN
+    --SCAN TABLE cat_toys
+        --LIST SUBQUERY 1
+            --SCAN TABLE cats
+        --LIST SUBQUERY 2
+            --SCAN TABLE toys
 
 -- What do your results mean?
 
     -- Was this a SEARCH or SCAN?
-
+        -- this uses scan
 
     -- What does that mean?
+    -- it means indexes can be used to improve efficiency
 
 
 
@@ -38,8 +74,16 @@
 
     -- Your code here
 
--- Paste your results below (as a comment):
+    -- SELECT COUNT(*) FROM cat_toys 
+    -- WHERE cat_id IN (
+    --     SELECT id FROM cats WHERE color = 'Olive'
+    -- ) AND toy_id IN (
+    --     SELECT id FROM toys WHERE price > 55
+    -- );
 
+-- Paste your results below (as a comment):
+-- 215
+-- Run Time: real 0.003 user 0.002993 sys 0.000000
 
 
 
@@ -50,16 +94,33 @@
 -- Create index:
 
     -- Your code here
+    -- CREATE INDEX idx_cats_color ON cats (color);
+    -- CREATE INDEX idx_toys_price ON toys (price);
+    -- CREATE INDEX idx_cat_toys_cat_id_toy_id ON cat_toys (cat_id, toy_id);
 
 -- Analyze Query:
     -- Your code here
+    -- EXPLAIN QUERY PLAN
+    -- SELECT COUNT(*) FROM cat_toys 
+    -- WHERE cat_id IN (
+    --     SELECT id FROM cats WHERE color = 'Olive'
+    -- ) AND toy_id IN (
+    --     SELECT id FROM toys WHERE price > 55
+    -- );
 
 -- Paste your results below (as a comment):
 
+-- QUERY PLAN
+-- |--SEARCH TABLE cat_toys USING COVERING INDEX idx_cat_toys_cat_id_toy_id (cat_id=? AND toy_id=?)
+-- |--LIST SUBQUERY 1
+-- |  `--SEARCH TABLE cats USING COVERING INDEX idx_cats_color (color=?)
+-- `--LIST SUBQUERY 2
+--    `--SEARCH TABLE toys USING COVERING INDEX idx_toys_price (price>?)
 
 -- Analyze Results:
 
     -- Is the new index being applied in this query?
+    -- yes, it is being used instead of the scans
 
 
 
@@ -71,18 +132,29 @@
 
     -- Your code here
 
+    SELECT COUNT(*) FROM cat_toys 
+    WHERE cat_id IN (
+        SELECT id FROM cats WHERE color = 'Olive'
+    ) AND toy_id IN (
+        SELECT id FROM toys WHERE price > 55
+    );
+
 -- Paste your results below (as a comment):
+-- 215
+-- Run Time: real 0.132 user 0.130969 sys 0.000414
 
 
 -- Analyze Results:
     -- Are you still getting the correct query results?
+    -- yes
 
 
     -- Did the execution time improve (decrease)?
+    -- no, it decreased
 
 
     -- Do you see any other opportunities for making this query more efficient?
-
+    -- removing indexes may respeed up performance
 
 
 ---------------------------------
