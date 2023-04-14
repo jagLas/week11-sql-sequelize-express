9ff9629f11b0f7860e43c8054eaef4b82b3eb816
jagLas <jglasher@gmail.com>
2023-04-14 16:10:36 -0400

    phase 7 complete

--- practice-for-week-11-combining-express-sequelize-long-practice-3/server/routes/classrooms.js
+++ practice-for-week-11-combining-express-sequelize-long-practice-3/server/routes/classrooms.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import model(s)
-const { Classroom, Supply, StudentClassroom, sequelize } = require('../db/models');
+const { Classroom, Supply, StudentClassroom, Student, sequelize } = require('../db/models');
 const { Op } = require('sequelize');
 
 // List of classrooms
@@ -90,7 +90,24 @@ router.get('/:id', async (req, res, next) => {
             // Include students of the classroom and order students by lastName
                 // then firstName (both in ascending order)
                 // (Optional): No need to include the StudentClassrooms
-        // Your code here
+        // Your code here,
+        include: [
+            {
+                model: Supply,
+                attributes: ['id', 'name', 'category', 'handed']
+            },
+            {
+                model: Student,
+                attributes: ['id', 'firstName', 'lastName', 'leftHanded'],
+                through: {
+                    attributes: []
+                }
+            }
+        ],
+        order: [
+            [Supply, 'Category'], [Supply, 'name'],
+            [Student, 'lastName'], [Student, 'firstName']
+        ]
     });
 
     if (!classroom) {
